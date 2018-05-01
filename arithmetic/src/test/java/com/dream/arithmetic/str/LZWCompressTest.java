6894ec2b4da2eca5a4141ac516727f98c0dbfadf
小村长 <592657253@qq.com>
2018-05-01 23:06:35 +0800

    u

---
+++ arithmetic/src/test/java/com/dream/arithmetic/str/LZWCompressTest.java
@@ -0,0 +1,11 @@
+package com.dream.arithmetic.str;
+
+public class LZWCompressTest {
+	
+	  
+    public void main(String[] args) {
+        LZWCompress.compress();
+        LZWCompress.expand();
+    }
+
+}
