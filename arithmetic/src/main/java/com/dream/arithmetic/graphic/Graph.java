b40048353bfe130d6b32f6edacaff78c9c51aea8
小村长 <592657253@qq.com>
2018-05-30 00:36:52 +0800

    u

---
+++ arithmetic/src/main/java/com/dream/arithmetic/graphic/Graph.java
@@ -0,0 +1,36 @@
+package com.dream.arithmetic.graphic;
+
+import java.io.IOException;
+
+public interface Graph {
+	float INFINITY = 9999;
+
+	enum GraphType {
+		directed, undirected
+	}
+
+	void initialize(int v, int e);
+
+	void createGraph(String fileName) throws IOException;
+
+	void print();
+
+	int V();
+
+	int E();
+
+	int degree(int v);
+
+	int maxDegree();
+
+	float weight(int v1, int v2);
+
+	boolean isAdjacent(int v1, int v2);
+
+	void dfs(int v);
+
+	void bfs(int v);
+
+	float shortestPath(int s, int t);
+
+}
